9d452d98412d50422fd09d532f6609761371b7cf
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-04-14 14:36:14 -0400

    Footer style, no position

--- dev/wp-content/themes/bones/footer.php
+++ dev/wp-content/themes/bones/footer.php
@@ -2,12 +2,18 @@
 
 				<div id="inner-footer" class="wrap clearfix">
 
+					<p class="copyright">
+						Copyright &copy; <?php echo date('Y'); ?> Guardian Automotive Glass, All Rights Reserved.
+					</p>
+
+					<div id="footer-logo">
+						<img src="<?php echo get_template_directory_uri(); ?>/library/images/footer/guardian_logo.png" />
+					</div>
+
 					<nav role="navigation">
-							<?php bones_footer_links(); ?>
-									</nav>
-
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-
+						<?php bones_footer_links(); ?>
+					</nav>
+					
 				</div> <!-- end #inner-footer -->
 
 			</footer> <!-- end footer -->
