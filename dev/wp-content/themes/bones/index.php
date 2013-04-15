a9d86b2b47fbe5786d65dae0209da63c5648cc99
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-04-15 14:07:36 -0400

    Slider overflow fix

--- dev/wp-content/themes/bones/index.php
+++ dev/wp-content/themes/bones/index.php
@@ -5,7 +5,7 @@
 				<div id="inner-content" class="wrap clearfix">
 
 						<div id="main" class="eightcol first clearfix" role="main">
-
+							
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
