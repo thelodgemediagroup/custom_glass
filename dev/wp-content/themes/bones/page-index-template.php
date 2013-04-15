1790ea0e20bef9ec57da5c443f062f16c2cc9abc
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-04-15 14:26:18 -0400

    New static index page

---
+++ dev/wp-content/themes/bones/page-index-template.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: CGS Index Page
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap clearfix">
+
+					<div id="main" class="eightcol first clearfix" role="main">
+					
+					</div> <!-- end #main -->
+
+				<?php get_sidebar(); ?>
+
+				</div> <!-- end #inner-content -->
+
+			</div> <!-- end #content -->					
+
+<?php get_footer(); ?>
