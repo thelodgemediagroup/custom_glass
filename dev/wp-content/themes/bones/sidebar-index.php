c59055ede464e06038b5bc7e37eb41168a7ef106
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-04-16 12:48:58 -0400

    Img/p wordwrap sidebar

--- dev/wp-content/themes/bones/sidebar-index.php
+++ dev/wp-content/themes/bones/sidebar-index.php
@@ -4,30 +4,16 @@
 
 						<h3>News</h3>
 
-						<img src="<?php echo get_template_directory_uri(); ?>/library/images/sidebar_index/news.jpg" />
+						<div class="sidebar-img-txt">
 
-						<p>Nimi, ni rem il imus excepellabo. Abor sincid et quisita taes cid jszribus nonseq Read More></p>
+							<img src="<?php echo get_template_directory_uri(); ?>/library/images/sidebar_index/news.jpg" />
 
-					</div> <!-- .sidebar-list -->
+							<p>Nimi, ni rem il imus excepellabo. Abor sincid et quisita taes cid jszribus nonseq Read More></p>
 
-					<div class="sidebar-list">
-
-						<h3>Capabilities</h3>
-
-						<img src="<?php echo get_template_directory_uri(); ?>/library/images/sidebar_index/capabilities.jpg" />
-
-						<p>Cimi, ni rem il imus excepellabo. Abor sincid et quisita taes cid jszribus nonseq Read More></p>
+						</div> <!-- .sidebar-img-txt -->
 
 					</div> <!-- .sidebar-list -->
 
-					<div class="sidebar-list">
-
-						<h3>Advanced Technologies</h3>
-
-						<img src="<?php echo get_template_directory_uri(); ?>/library/images/sidebar_index/technologies.jpg" />
-
-						<p>Timi, ni rem il imus excepellabo. Abor sincid et quisita taes cid jszribus nonseq Read More></p>
-
-					</div> <!-- .sidebar-list -->										
+											
 
 				</div> <!-- #sidebar1 -->
