9fa766085c2e3de2048df1f45ffe678f7386ed33
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-04-16 14:30:56 -0400

    Social media icon replatement/reposition

--- dev/wp-content/themes/bones/header.php
+++ dev/wp-content/themes/bones/header.php
@@ -56,11 +56,11 @@
 					<div id="header-float-block">
 
 						<div id="header-social-media">
-							<a href="http://www.linkedin.com" title="LinkedIn"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/linkedin_btn.png" /></a>
-							<a href="http://www.twitter.com" title="Twitter"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/twitter_btn.png" /></a>
-							<a href="http://www.facebook.com" title="Facebook"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/facebook_btn.png" /></a>
-							<a href="http://www.pintrest.com" title="Pintrest"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/pintrest_btn.png" /></a>
-							<a href="http://www.youtube.com" title="You Tube"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/youtube_btn.png" /></a>
+							<a href="http://www.linkedin.com" title="LinkedIn"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/linkedin.png" /></a>
+							<a href="http://www.twitter.com" title="Twitter"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/twitter.png" /></a>
+							<a href="http://www.facebook.com" title="Facebook"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/facebook.png" /></a>
+							<a href="http://www.pintrest.com" title="Pintrest"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/pintrest.png" /></a>
+							<a href="http://www.youtube.com" title="You Tube"><img src="<?php echo get_template_directory_uri(); ?>/library/images/buttons/youtube.png" /></a>
 						</div> <!--# header-social-media -->
 
 						<div id="header-search">
